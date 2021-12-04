f4e65e65bf5b591dcc1d9edade3475bd41a984ed
Aarox3 <poiuytrewq1990@o2.pl>
2021-12-04 18:02:55 +0100

    First version

---
+++ Diary/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Diary.Views;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace Diary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const int minSplTime = 4000;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SplashScreen splash = new SplashScreen();
+            splash.Show();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            base.OnStartup(e);
+            MainWindow main = new MainWindow();
+            timer.Stop();
+            int remainingTime = minSplTime - (int)timer.ElapsedMilliseconds;
+            if (remainingTime > 0)
+                Thread.Sleep(remainingTime);
+            splash.Close();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var metroWindow = Current.MainWindow as MetroWindow;
+            metroWindow.ShowMessageAsync("Nieoczekiwany  wyjątek",
+                "Wystąpił nieoczekiwany wyjątek." + 
+                Environment.NewLine +
+                e.Exception.Message);
+
+            e.Handled = true;
+        }
+    }
+}
